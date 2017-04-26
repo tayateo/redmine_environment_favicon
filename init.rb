e534de33680e07edf9e11e5edab4884682a12754
a.fedorova <a.fedorova@elins.ru>
2017-04-26 16:05:10 +0300

    commented dependencies of the plugin

--- init.rb
+++ init.rb
@@ -6,9 +6,9 @@ Redmine::Plugin.register plugin_name do
   author 'ELINS'
   version '0.0.1'
 
-  requires_redmine :version_or_higher => '2.5.0'
-  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
-  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
+  # requires_redmine :version_or_higher => '2.5.0'
+  # requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
+  # requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
 
 end
 
